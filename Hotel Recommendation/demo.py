5ddc620e961a06922c8ff8883b68c7f27978d609
郭鹏 <532957426@qq.com>
2016-05-09 17:19:41 +0800

    data path change

--- Hotel Recommendation/demo.py
+++ Hotel Recommendation/demo.py
@@ -1,6 +1,6 @@
 # -*- coding: UTF-8 -*-
 import os
 
-print os.path.abspath('../../kaggle data')
-with open(os.path.abspath('../../kaggle data/test'), 'w')as f:
+print os.path.abspath('../../kaggle data/hotel_recommendation')
+with open(os.path.abspath('../../kaggle data/hotel_recommendation/test'), 'w')as f:
     f.write('Hello World')
