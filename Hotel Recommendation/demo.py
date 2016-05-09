653b4e8674d620c48c6dee97f7884d8e516d742c
guopeng <532957426@qq.com>
2016-05-09 22:04:02 +0800

    test commit in windows

--- Hotel Recommendation/demo.py
+++ Hotel Recommendation/demo.py
@@ -3,4 +3,4 @@ import os
 
 print os.path.abspath('../../kaggle data/hotel_recommendation')
 with open(os.path.abspath('../../kaggle data/hotel_recommendation/test'), 'w')as f:
-    f.write('Hello World')
+    f.write('test')
