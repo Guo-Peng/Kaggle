36381f175af65ae952fd75a3d14e522e9ceb9b68
郭鹏 <532957426@qq.com>
2016-05-09 11:29:10 +0800

    data path

--- Hotel Recommendation/demo.py
+++ Hotel Recommendation/demo.py
@@ -1,2 +1,6 @@
 # -*- coding: UTF-8 -*-
-print 'hello world'
+import os
+
+print os.path.abspath('../../kaggle data')
+with open(os.path.abspath('../../kaggle data/test'), 'w')as f:
+    f.write('Hello World')
